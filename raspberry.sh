090b0d1c233729fa71bfa47a50f7f448ea60e628
kali <kali@kalis-MBP.home>
2017-04-12 10:29:10 +0200

    adding more -y

--- raspberry.sh
+++ raspberry.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-sudo apt-get update
+sudo apt-get update -y
 sudo apt-get upgrade -y
-sudo apt-get install wavemon
-sudo apt-get install screen
-sudo apt-get install vim
+sudo apt-get install wavemon -y
+sudo apt-get install screen -y
+sudo apt-get install vim -y
 sudo apt-get install git -y
-sudo apt-get install dnsutils
-sudo apt-get install htop
+sudo apt-get install dnsutils -y
+sudo apt-get install htop -y
 mdkir .vim
 mkdir .vim/colors
 cd .vim/colors
