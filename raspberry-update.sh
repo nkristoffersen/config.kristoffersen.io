4f94cfdd205dbbc5be7809c1c44162e9474b5caa
Nicholas Kristoffersen <nick@kristoffersen.io>
2017-06-28 15:07:40 +0200

    moving timestamp to end of email subject

--- raspberry-update.sh
+++ raspberry-update.sh
@@ -3,6 +3,6 @@ echo "starting rpi update!"
 sudo apt-get update && sudo apt-get dist-upgrade -y
 sudo apt-get autoremove -y
 sudo apt-get autoclean -y
-sendemail -l email.log -f "$FROMEMAIL" -u "$PISERIAL $TIMESTAMP syslog" -t "$TOEMAIL" -s "smtp.gmail.com:587" -o tls=yes -xu "$FROMEMAIL" -xp "$EMAILPASSWORD" -o message-file="/var/log/syslog"
-sendemail -l email.log -f "$FROMEMAIL" -u "$PISERIAL $TIMESTAMP wavelog" -t "$TOEMAIL" -s "smtp.gmail.com:587" -o tls=yes -xu "$FROMEMAIL" -xp "$EMAILPASSWORD" -o message-file="/var/log/wave.log"
+sendemail -l email.log -f "$FROMEMAIL" -u "$PISERIAL syslog $TIMESTAMP" -t "$TOEMAIL" -s "smtp.gmail.com:587" -o tls=yes -xu "$FROMEMAIL" -xp "$EMAILPASSWORD" -o message-file="/var/log/syslog"
+sendemail -l email.log -f "$FROMEMAIL" -u "$PISERIAL wavelog $TIMESTAMP" -t "$TOEMAIL" -s "smtp.gmail.com:587" -o tls=yes -xu "$FROMEMAIL" -xp "$EMAILPASSWORD" -o message-file="/var/log/wave.log"
 echo "finished rpi update!"
