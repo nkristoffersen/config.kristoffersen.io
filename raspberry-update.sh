0ee36778096dfe92cca6123a4b36aa16d0cbd5b5
Nicholas Kristoffersen <nick@kristoffersen.io>
2017-06-28 15:16:54 +0200

    adding sudo just for the hell of it

--- raspberry-update.sh
+++ raspberry-update.sh
@@ -3,6 +3,6 @@ echo "starting rpi update!"
 sudo apt-get update && sudo apt-get dist-upgrade -y
 sudo apt-get autoremove -y
 sudo apt-get autoclean -y
-sendemail -l email.log -f "$FROMEMAIL" -u "$PISERIAL syslog $TIMESTAMP" -t "$TOEMAIL" -s "smtp.gmail.com:587" -o tls=yes -xu "$FROMEMAIL" -xp "$EMAILPASSWORD" -o message-file="/var/log/syslog"
-sendemail -l email.log -f "$FROMEMAIL" -u "$PISERIAL wavelog $TIMESTAMP" -t "$TOEMAIL" -s "smtp.gmail.com:587" -o tls=yes -xu "$FROMEMAIL" -xp "$EMAILPASSWORD" -o message-file="/var/log/wave.log"
+sudo sendemail -l email.log -f "$FROMEMAIL" -u "$PISERIAL syslog $TIMESTAMP" -t "$TOEMAIL" -s "smtp.gmail.com:587" -o tls=yes -xu "$FROMEMAIL" -xp "$EMAILPASSWORD" -m "see attached" -a "/var/log/syslog"
+sudo sendemail -l email.log -f "$FROMEMAIL" -u "$PISERIAL wavelog $TIMESTAMP" -t "$TOEMAIL" -s "smtp.gmail.com:587" -o tls=yes -xu "$FROMEMAIL" -xp "$EMAILPASSWORD" -m "see attached" -a "/var/log/wave.log"
 echo "finished rpi update!"
