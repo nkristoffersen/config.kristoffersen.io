6e6e4bdea5293a21f6a0b5306154e7810d1362f1
Nicholas Kristoffersen <nick@kristoffersen.io>
2017-06-29 12:57:07 +0200

    printing variables

--- raspberry-config.sh
+++ raspberry-config.sh
@@ -13,8 +13,6 @@ sudo apt-get install -y autossh
 sudo apt-get install -y dnsmasq
 sudo apt-get install -y sendemail
 sudo apt-get remove -y nano
-sudo apt-get install -y unattended-upgrades
-sudo apt-get install -y apt-listchanges
 wget http://node-arm.herokuapp.com/node_latest_armhf.deb && sudo dpkg -i node_latest_armhf.deb
 sudo apt-get install -y libudev-dev
 sudo npm install forever -g
@@ -23,7 +21,9 @@ sudo curl -o /usr/local/bin/rmate https://raw.githubusercontent.com/aurora/rmate
 sudo chmod +x /usr/local/bin/rmate
 mkdir .vim
 mkdir .vim/colors
+mkdir .vim/autoload
 wget https://raw.githubusercontent.com/joshdick/onedark.vim/master/colors/onedark.vim && mv onedark.vim .vim/colors/onedark.vim
+wget https://raw.githubusercontent.com/joshdick/onedark.vim/master/autoload/onedark.vim && mv onedark.vim .vim/autoload/onedark.vim
 
 sudo echo "FROMEMAIL=emailaddress@gmail.com" >> /etc/profile
 sudo echo "TOEMAIL=emailaddress@gmail.com" >> /etc/profile
--- raspberry-update.sh
+++ raspberry-update.sh
@@ -3,6 +3,11 @@ echo "starting rpi update!"
 sudo apt-get update && sudo apt-get dist-upgrade -y
 sudo apt-get autoremove -y
 sudo apt-get autoclean -y
+echo $FROMEMAIL
+echo $PISERIAL
+echo $TIMESTAMP
+echo $TOEMAIL
+echo $EMAILPASSWORD
 sudo sendemail -l email.log -f "$FROMEMAIL" -u "$PISERIAL syslog $TIMESTAMP" -t "$TOEMAIL" -s "smtp.gmail.com:587" -o tls=yes -xu "$FROMEMAIL" -xp "$EMAILPASSWORD" -m "see attached" -a "/var/log/syslog"
 sudo sendemail -l email.log -f "$FROMEMAIL" -u "$PISERIAL wavelog $TIMESTAMP" -t "$TOEMAIL" -s "smtp.gmail.com:587" -o tls=yes -xu "$FROMEMAIL" -xp "$EMAILPASSWORD" -m "see attached" -a "/var/log/wave.log"
 echo "finished rpi update!"
